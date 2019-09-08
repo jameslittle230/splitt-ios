d00cf27c705e07188dfef5c43415a02b47af24e0
James Little <littleguy23@gmail.com>
2019-09-07 19:14:56 -0700

    Initial Commit

---
+++ Splitt iOSTests/Splitt_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Splitt_iOSTests.swift
+//  Splitt iOSTests
+//
+//  Created by James Little on 9/7/19.
+//  Copyright © 2019 Splitt. All rights reserved.
+//
+
+import XCTest
+@testable import Splitt_iOS
+
+class Splitt_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
