d80ca7053063f074782acc5067bcea735a8bf531
James Little <littleguy23@gmail.com>
2019-09-28 17:58:24 -0700

    Initial commit

---
+++ Splitt iOS/KeyboardResponder.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+func endEditing() {
+    let keyWindow = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+    keyWindow?.endEditing(true)
+}
--- Splitt iOS/SceneDelegate.swift
+++ Splitt iOS/SceneDelegate.swift
@@ -20,7 +20,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let history = [
+            HistoryElement(time: "5 minutes ago", who: "James", action: "reconciled 30 splits"),
+            HistoryElement(time: "5 minutes ago", who: "James", action: "reconciled 30 splits"),
+            HistoryElement(time: "5 minutes ago", who: "James", action: "reconciled 30 splits"),
+            HistoryElement(time: "5 minutes ago", who: "James", action: "reconciled 30 splits"),
+            HistoryElement(time: "5 minutes ago", who: "James", action: "reconciled 30 splits"),
+            HistoryElement(time: "5 minutes ago", who: "James", action: "reconciled 30 splits"),
+            HistoryElement(time: "5 minutes ago", who: "James", action: "reconciled 30 splits"),
+            HistoryElement(time: "5 minutes ago", who: "James", action: "reconciled 30 splits"),
+            HistoryElement(time: "5 minutes ago", who: "James", action: "reconciled 30 splits"),
+        ]
+        let contentView = ContentView(historyElements: history)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
